5f2bfbfd6fb8e7a2541506455e1ace24f82a6b3f
drdlee <danielrudi.lee@gmail.com>
2018-01-01 02:12:10 +0800

    post_new: Action, reduxForm, formReducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 export const FETCH_POSTS = 'FETCH_POSTS';
+export const CREATE_POST = 'CREATE_POST';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api/posts';
 const API_KEY = '?key=qwerty35';
@@ -9,6 +10,14 @@ export function fetchPosts(){
   const request = axios.get(`${ROOT_URL}${API_KEY}`);
   return {
     type: FETCH_POSTS,
+    payload: request
+  }
+}
+
+export function createPost(data){
+  const request = axios.post(`${ROOT_URL}${API_KEY}`, data);
+  return {
+    type: CREATE_POST,
     payload: request
   }
 }
--- src/components/post_new.js
+++ src/components/post_new.js
@@ -1,23 +1,37 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
+import { reduxForm } from 'redux-form';
+import { createPost } from '../actions/index';
+import PropTypes from 'prop-types';
 
 class PostNew extends Component {
+  static contextTypes = {
+    router: PropTypes.object
+  }
+  onSubmitYo(props){
+    this.props.createPost(props)
+    .then(()=>{
+      this.context.router.push('/')
+    });
+  }
+
   render(){
+    const { handleSubmit, fields: { title, categories, content } } = this.props;
     return (
       <div>
         <h3>Create A New Post</h3>
-        <form>
+        <form onSubmit={handleSubmit(this.onSubmitYo.bind(this))}>
           <div className="form-group">
             <label>Title</label>
-            <input type="text" className="form-control" />
+            <input type="text" className="form-control" {...title}/>
           </div>
           <div className="form-group">
             <label>Categories</label>
-            <input type="text" className="form-control" />
+            <input type="text" className="form-control" {...categories}/>
           </div>
           <div className="form-group">
             <label>Content</label>
-            <textarea className="form-control" />
+            <textarea className="form-control" {...content}/>
           </div>
           <button type="submit" className="btn btn-primary">Submit</button>
           <Link to="/" className="btn btn-danger">Cancel</Link>
@@ -27,4 +41,7 @@ class PostNew extends Component {
   }
 }
 
-export default PostNew;
+export default reduxForm ({
+  form: 'PostNewForm',
+  fields: [ 'title', 'categories', 'content' ]
+}, null, { createPost }) (PostNew);
